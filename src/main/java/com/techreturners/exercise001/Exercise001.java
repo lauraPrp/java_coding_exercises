2a7fb62995267dc824dc4f8aeb32965f7b8a6691
lauraPrp <laura.porpiglia@gmail.com>
2021-12-06 18:22:19 +0000

    ex 1

--- src/main/java/com/techreturners/exercise001/Exercise001.java
+++ src/main/java/com/techreturners/exercise001/Exercise001.java
@@ -1,30 +1,40 @@
 package com.techreturners.exercise001;
 
 import java.util.List;
+import java.lang.Math;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 
 public class Exercise001 {
     public String capitalizeWord(String word) {
-        // Add your code here
-        return "";
+    	
+        return word.substring(0,1).toUpperCase().concat(word.substring(1));
+    
     }
 
     public String generateInitials(String firstName, String lastName) {
-        // Add your code here
-        return "";
+      String initials= firstName.substring(0,1).toUpperCase().concat(".").concat(lastName.substring(0,1).toUpperCase()); 
+       return initials;
     }
 
     public double addVat(double originalPrice, double vatRate) {
-        // Add your code here
-        return 0.0;
+    	double valDouble1= originalPrice+(originalPrice/100*vatRate);
+    	BigDecimal bd = new BigDecimal(valDouble1);
+    	BigDecimal bd2 = bd.setScale(2, RoundingMode.HALF_UP);
+    	double res = bd2.doubleValue();
+    	
+	return res;
     }
 
     public String reverse(String sentence) {
-        // Add your code here
-        return "";
+      StringBuilder sb= new StringBuilder(sentence) ;
+        return sb.reverse().toString();
     }
 
     public int countLinuxUsers(List<User> users) {
-        // Add your code here
-        return 0;
+    	int numUsers = (int) users.stream()
+        .filter(s -> s.getType().equals("Linux")).count();
+        return numUsers;
     }
 }
