c58c6dfb907a852f5140a5192460047a05cfc51d
lauraPrp <laura.porpiglia@gmail.com>
2021-12-07 23:18:26 +0000

    ex004 completed, tests with excetions added

--- src/main/java/com/techreturners/exercise004/Exercise004.java
+++ src/main/java/com/techreturners/exercise004/Exercise004.java
@@ -1,23 +1,44 @@
 package com.techreturners.exercise004;
 
-import java.time.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 
 public class Exercise004 {
 
     LocalDateTime ldLater;
-    public static  long gigasec= 1_000_000_000L;
+    public static final long gigasec= 1_000_000_000L;
+
+    public LocalDate parseStrToLocalDate(String strToParse){
+        if(strToParse.length()!=8)
+            throw new IllegalArgumentException("Date not valid check input: it should be YYYYMMDD");
+        LocalDate date= LocalDate.parse(strToParse, DateTimeFormatter.BASIC_ISO_DATE);
+
+       return date;
+    }
+
+
+    public Exercise004(String dateString) {
+        LocalDate date= parseStrToLocalDate(dateString);
+        LocalTime lt = LocalTime.of(0,0,0);
+        ldLater = LocalDateTime.of(date,lt).plus(gigasec, ChronoUnit.SECONDS);
+
+    }
 
     public Exercise004(LocalDate date) {
         LocalTime lt = LocalTime.of(0,0,0);
         ldLater = LocalDateTime.of(date,lt).plus(gigasec, ChronoUnit.SECONDS);
+
     }
 
     public Exercise004(LocalDateTime dateTime) {
          ldLater = dateTime.plus(gigasec, ChronoUnit.SECONDS) ;
+
     }
 
     public LocalDateTime getDateTime() {
-    return   ldLater;
+        return   ldLater;
     }
 }
--- src/test/java/com/techreturners/exercise004/Exercise004Test.java
+++ src/test/java/com/techreturners/exercise004/Exercise004Test.java
@@ -1,13 +1,14 @@
 package com.techreturners.exercise004;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.Month;
+import java.time.format.DateTimeParseException;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class Exercise004Test {
 
@@ -20,7 +21,7 @@ public class Exercise004Test {
         assertEquals(expected, ex004.getDateTime());
     }
 
-    @Ignore("You can remove this @ignore annotation to run the test")
+
     @Test
     public void checkGetDateTimeWhenBothDateAndTimeIsSpecified() {
 
@@ -30,14 +31,39 @@ public class Exercise004Test {
         assertEquals(expected, ex004.getDateTime());
     }
 
-    @Ignore("You can remove this @ignore annotation to run the test")
+
     @Test
     public void checkGetDateTimeWhenBothDateAndTimeIsSpecifiedWithDayRollOver() {
 
         Exercise004 ex004 = new Exercise004(LocalDateTime.of(2021, Month.JANUARY, 24, 23, 59, 59, 0));
-        LocalDateTime expected = LocalDateTime.of(2052, Month.OCTOBER, 03, 1, 46, 39);
+        LocalDateTime expected = LocalDateTime.of(2052, Month.OCTOBER, 3, 1, 46, 39);
 
         assertEquals(expected, ex004.getDateTime());
     }
+    @Test
+    public void checkGetDateTimeWithString() { //String Format YYYYMMDD
+
+        Exercise004 ex004 = new Exercise004("20210719");
+        LocalDateTime expected = LocalDateTime.of(2053, Month.MARCH, 27, 1, 46, 40);
+
+        assertEquals(expected, ex004.getDateTime());
+    }
+
+    @Test(expected = DateTimeParseException.class)
+    public void checkGetDateTimeWhithStringWrongDay() { //String Format YYYYMMDD
+
+        Exercise004 ex004 = new Exercise004("20210755");
+        LocalDateTime expected = LocalDateTime.of(2053, Month.MARCH, 27, 1, 46, 40);
+
+        assertNotEquals(expected,ex004.getDateTime());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void checkGetDateTimeWhithStringWrong2() { //String Format YYYYMMDD
+
+        Exercise004 ex004 = new Exercise004("2021055755");
+}
+
+
 
 }
