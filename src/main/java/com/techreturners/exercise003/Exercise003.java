02910d657ee7cfbebaaeaf625fc501a2ecc05e6d
lauraPrp <laura.porpiglia@gmail.com>
2021-12-07 02:45:17 +0000

    done

--- src/main/java/com/techreturners/exercise003/Exercise003.java
+++ src/main/java/com/techreturners/exercise003/Exercise003.java
@@ -10,4 +10,9 @@ public class Exercise003 {
         throw new UnsupportedOperationException(("You can delete this statement and add your code here."));
     }
 
+    String[] pickMultipleIceCreamFlavours(){
+        String[] result = {"Pistachio", "Raspberry Ripple", "Vanilla", "Mint Chocolate Chip", "Chocolate", "Mango Sorbet"};
+        return result;
+    }
+
 }
