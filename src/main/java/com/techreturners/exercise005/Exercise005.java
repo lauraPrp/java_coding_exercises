8064a2dc53d2116a575071d1eb11d7f922374f2f
lauraPrp <laura.porpiglia@gmail.com>
2021-12-07 21:19:24 +0000

    ex005 completed

--- src/main/java/com/techreturners/exercise005/Exercise005.java
+++ src/main/java/com/techreturners/exercise005/Exercise005.java
@@ -3,20 +3,19 @@ package com.techreturners.exercise005;
 import java.util.Locale;
 
 public class Exercise005 {
-    private static 	String loAlphabet="abcdefghijklmnopqrstuvwxyz";
+    private static final String loAlphabet="abcdefghijklmnopqrstuvwxyz";
 
 
     public boolean isPangram(String input) {
        boolean noitsnot=true;
        input = input.toLowerCase(Locale.ROOT);
-       int[] check = new int[26];
-       for(int i =0;i<26;i++){
+        for(int i =0;i<26;i++){
           char letter = loAlphabet.charAt(i);
           if(input.indexOf(letter)==-1){
-              noitsnot =false;
+               return false;
           }
        }
-              return noitsnot;
+        return noitsnot;
     }
 
 }
