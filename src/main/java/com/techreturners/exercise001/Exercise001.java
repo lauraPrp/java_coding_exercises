a9161d9a94628682871503fb38b962c239a7b7f8
lauraPrp <laura.porpiglia@gmail.com>
2021-12-08 15:05:19 +0000

    refactoring: removed useless variable assignment

--- src/main/java/com/techreturners/exercise001/Exercise001.java
+++ src/main/java/com/techreturners/exercise001/Exercise001.java
@@ -1,40 +1,34 @@
 package com.techreturners.exercise001;
 
-import java.util.List;
-import java.lang.Math;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
-import java.text.DecimalFormat;
+import java.util.List;
 
 public class Exercise001 {
     public String capitalizeWord(String word) {
-
-        return word.substring(0,1).toUpperCase().concat(word.substring(1));
-
+        return word.substring(0, 1).toUpperCase().concat(word.substring(1));
     }
 
     public String generateInitials(String firstName, String lastName) {
-      String initials= firstName.substring(0,1).toUpperCase().concat(".").concat(lastName.substring(0,1).toUpperCase());
-       return initials;
+        return firstName.substring(0, 1).toUpperCase().concat(".").concat(lastName.substring(0, 1).toUpperCase());
     }
 
     public double addVat(double originalPrice, double vatRate) {
-    	double valDouble1= originalPrice+(originalPrice/100*vatRate);
-    	BigDecimal bd = new BigDecimal(valDouble1);
-    	BigDecimal bd2 = bd.setScale(2, RoundingMode.HALF_UP);
-    	double res = bd2.doubleValue();
-
-	return res;
+        double valDouble1 = originalPrice + (originalPrice / 100 * vatRate);
+        BigDecimal bd = new BigDecimal(valDouble1);
+        BigDecimal bd2 = bd.setScale(2, RoundingMode.HALF_UP);
+        
+        return bd2.doubleValue();
     }
 
     public String reverse(String sentence) {
-      StringBuilder sb= new StringBuilder(sentence) ;
+        StringBuilder sb = new StringBuilder(sentence);
         return sb.reverse().toString();
     }
 
     public int countLinuxUsers(List<User> users) {
-    	int numUsers = (int) users.stream()
-        .filter(s -> s.getType().equals("Linux")).count();
-        return numUsers;
+        return (int) users.stream()
+                .filter(s -> s.getType().equals("Linux"))
+                .count();
     }
 }
