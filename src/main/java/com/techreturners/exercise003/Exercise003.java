5ea831d9a2b65e0a77808aa48efdb25e73fef81a
lauraPrp <laura.porpiglia@gmail.com>
2021-12-10 16:27:20 +0000

    correct indentation

--- src/main/java/com/techreturners/exercise003/Exercise003.java
+++ src/main/java/com/techreturners/exercise003/Exercise003.java
@@ -5,47 +5,48 @@ import java.util.LinkedList;
 import java.util.Map;
 
 public class Exercise003 {
-    Map<String,Integer> flavours =  new HashMap<>() ;
-    String[] flavs= {"Pistachio","Raspberry Ripple","Vanilla","Mint Chocolate Chip","Chocolate","Mango Sorbet"};
+    Map<String, Integer> flavours = new HashMap<>();
+    String[] flavs = {"Pistachio", "Raspberry Ripple", "Vanilla", "Mint Chocolate Chip", "Chocolate", "Mango Sorbet"};
 
-    public Exercise003(){
+    public Exercise003() {
 
     }
 
-    public Exercise003(boolean myWay){
-        flavours.put("Pistachio",0);
-        flavours.put("Raspberry Ripple",1);
-        flavours.put("Vanilla",2);
-        flavours.put("Mint Chocolate Chip",3);
-        flavours.put("Chocolate",4);
-        flavours.put("Mango Sorbet",5);
+    public Exercise003(boolean myWay) {
+        flavours.put("Pistachio", 0);
+        flavours.put("Raspberry Ripple", 1);
+        flavours.put("Vanilla", 2);
+        flavours.put("Mint Chocolate Chip", 3);
+        flavours.put("Chocolate", 4);
+        flavours.put("Mango Sorbet", 5);
     }
 
     int getIceCreamCode(String iceCreamFlavour) {
-        for(int i=0;i<flavs.length;i++){
-            if(flavs[i].equals(iceCreamFlavour)){
+        for (int i = 0; i < flavs.length; i++) {
+            if (flavs[i].equals(iceCreamFlavour)) {
                 return i;
             }
         }
-    return 999;
+        return 999;
     }
 
     String[] iceCreamFlavours() {
-    	return flavs;
+        return flavs;
 
     }
 
     int getIceCreamCode(String iceCreamFlavour, boolean myWay) {
-        return  flavours.get(iceCreamFlavour);
+        return flavours.get(iceCreamFlavour);
     }
+
     String[] iceCreamFlavours(boolean myWay) {
-        String[] result=new String[6];
+        String[] result = new String[6];
         LinkedList<String> sortedList = new LinkedList<>();
 
         flavours.entrySet()
                 .stream()
                 .sorted(Map.Entry.comparingByValue())
-                   .forEachOrdered(x -> sortedList.add(x.getKey()));
+                .forEachOrdered(x -> sortedList.add(x.getKey()));
 
         return sortedList.toArray(new String[sortedList.size()]);
     }
--- src/test/java/com/techreturners/exercise003/Exercise003Test.java
+++ src/test/java/com/techreturners/exercise003/Exercise003Test.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
+
 public class Exercise003Test {
 
     private Exercise003 ex003;
@@ -16,27 +17,27 @@ public class Exercise003Test {
         ex003b = new Exercise003(true);
     }
 
-    
+
     @Test
     public void checkGetIceCreamCodeForMintChocolateChip() {
         String iceCreamFlavour = "Mint Chocolate Chip";
         int expected = 3;
 
         assertEquals(expected, ex003.getIceCreamCode(iceCreamFlavour));
-        assertEquals(expected, ex003b.getIceCreamCode(iceCreamFlavour,true));
+        assertEquals(expected, ex003b.getIceCreamCode(iceCreamFlavour, true));
     }
 
-    
+
     @Test
     public void checkGetIceCreamCodeForMangoSorbet() {
         String iceCreamFlavour = "Mango Sorbet";
         int expected = 5;
 
         assertEquals(expected, ex003.getIceCreamCode(iceCreamFlavour));
-        assertEquals(expected, ex003b.getIceCreamCode(iceCreamFlavour,true));
+        assertEquals(expected, ex003b.getIceCreamCode(iceCreamFlavour, true));
     }
 
-    
+
     @Test
     public void checkGetIceCreamCodeForRaspberryRipple() {
         String iceCreamFlavour = "Raspberry Ripple";
@@ -52,11 +53,11 @@ public class Exercise003Test {
         assertEquals(expected, ex003b.getIceCreamCode(iceCreamFlavour, true));
     }
 
-    
+
     @Test
     public void checkPickMultipleIceCreamFlavours() {
 
-        String[] expected = { "Pistachio", "Raspberry Ripple", "Vanilla", "Mint Chocolate Chip", "Chocolate", "Mango Sorbet" };
+        String[] expected = {"Pistachio", "Raspberry Ripple", "Vanilla", "Mint Chocolate Chip", "Chocolate", "Mango Sorbet"};
 //	assertEquals on Arrays is deprecated
         assertArrayEquals(expected, ex003.iceCreamFlavours());
         assertArrayEquals(expected, ex003b.iceCreamFlavours(true));
